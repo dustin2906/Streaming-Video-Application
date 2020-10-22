2c93f1ecac224550550f8c3d8e50b8dda6ab5b31
= <=>
2020-10-22 16:29:35 +0300

    Updates

---
+++ precache-manifest.efb208475d549f5853a54c326fded256.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "607bdbe7f02c6be1577a2c957a50ba28",
+    "url": "/Streaming-Video-Application/client/index.html"
+  },
+  {
+    "revision": "e19df30dbc8b4b0847d4",
+    "url": "/Streaming-Video-Application/client/static/js/2.79b7a91b.chunk.js"
+  },
+  {
+    "revision": "7ff3e8849394329239d343acaff0f565",
+    "url": "/Streaming-Video-Application/client/static/js/2.79b7a91b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "002d4ba9748d4f70e99d",
+    "url": "/Streaming-Video-Application/client/static/js/main.91f2e7c8.chunk.js"
+  },
+  {
+    "revision": "62898ecf81730f88f408",
+    "url": "/Streaming-Video-Application/client/static/js/runtime-main.a9ee461c.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Streaming-Video-Application/client/precache-manifest.efb208475d549f5853a54c326fded256.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Streaming-Video-Application/client/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
